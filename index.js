cd60680f9c3582b36768d1eefac632617a46b9e5
AkashKasimani <117535210+AkashKasimani@users.noreply.github.com>
2024-10-06 13:22:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+const goTopBtn = document.querySelector('.go-top-btn');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 200) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
+}
+
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
+
