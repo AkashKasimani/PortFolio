68c50054c963b284fb3a76711a897e9e7baa0dd3
Akash <117535210+AkashKasimani@users.noreply.github.com>
2025-01-19 00:12:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
-const goTopBtn = document.querySelector('.go-top-btn');
-
-window.addEventListener('scroll', checkHeight)
-
-function checkHeight(){
-  if(window.scrollY > 200) {
-    goTopBtn.style.display = "flex"
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  if (icon.classList.contains("bxs-navigation")) {
+    icon.classList.remove("bxs-navigation");
+    icon.classList.add("bx-navigation"); // Replace with a suitable close icon
   } else {
-    goTopBtn.style.display = "none"
+    icon.classList.remove("bx-navigation");
+    icon.classList.add("bxs-navigation");
+  }
+
+  icon.classList.toggle("open");
+}
+const goTopBtn = document.querySelector(".go-top-btn");
+
+window.addEventListener("scroll", checkHeight);
+
+function checkHeight() {
+  if (window.scrollY > 200) {
+    goTopBtn.style.display = "flex";
+  } else {
+    goTopBtn.style.display = "none";
   }
 }
 
-goTopBtn.addEventListener('click', () => {
+goTopBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
-  })
-})
-
+    behavior: "smooth",
+  });
+});
